c7c98439f41116866466209ab5e6d6dfdf63c164
Rahwa <rahwafesseha@gmail.com>
2022-08-11 10:42:34 +0100

    initial commit

---
+++ src/Form.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import _ from "lodash";
+
+const Form = () => {
+  const [username, setUsername] = useState("");
+  const [data, setData] = useState([]);
+  const [usedLanguages, setUsedLanguages] = useState("");
+  const languages = {};
+  const maxKey = _.max(
+    Object.keys(usedLanguages),
+    (usedLanguage) => usedLanguages[usedLanguage]
+  );
+
+  useEffect(() => {
+    if (username !== "") {
+      axios
+        .get(`https://api.github.com/users/${username}/repos`)
+        .then((res) => {
+          Object.entries(res.data).map(([key, value]) => {
+            console.log({ languages });
+            if (value.language === null) {
+              return "";
+            }
+            if (!languages.hasOwnProperty(value.language)) {
+              return (languages[value.language] = 1);
+            } else {
+              return (languages[value.language] = languages[
+                value.language
+              ] += 1);
+            }
+          });
+          setData(res.data);
+          setUsedLanguages(languages);
+        });
+    }
+  }, [username]);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setUsername(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    setUsername(username);
+  };
+
+  return (
+    <>
+      {data
+        ? Object.entries(data).map(([key, value]) => (
+            <div key={key}>
+              <p>{value.language}</p>
+            </div>
+          ))
+        : null}
+
+      {usedLanguages !== undefined && (
+        <div>
+          <h1>{maxKey}</h1>
+        </div>
+      )}
+
+      <form>
+        <fieldset>
+          <label>
+            <p>username</p>
+            <input
+              name="username"
+              onChange={(event) => handleChange(event)}
+              value={username}
+            />
+          </label>
+        </fieldset>
+        <button onClick={handleClick} type="submit">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Form;
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import searchMan from "../src/Images/searchMan.jpeg";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="header-image">
+        <img src={searchMan} alt="" />
+      </h1>
+      <h1 className="header">Favorite Language</h1>
+      <h4 className="description">
+        Enter a GitHub username to find out user's favorite programming
+        language.
+      </h4>
+    </div>
+  );
+};
+
+export default Header;
